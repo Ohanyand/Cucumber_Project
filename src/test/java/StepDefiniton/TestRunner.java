be6a5b8492ad415ce22b6e81287d6750bb32b13f
Davit <ohanyand21@gmail.com>
2021-02-20 21:46:29 +0400

    adding html reports

--- src/test/java/StepDefiniton/TestRunner.java
+++ src/test/java/StepDefiniton/TestRunner.java
@@ -5,6 +5,6 @@ import org.junit.runner.RunWith;
 import io.cucumber.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features= "src/test/resources/features", glue ={"StepDefiniton"}, monochrome = true)
+@CucumberOptions(features= "src/test/resources/features", glue ={"StepDefiniton"}, monochrome = true,plugin = {"pretty","html:target/htmlReports"})
 public class TestRunner {
 }
